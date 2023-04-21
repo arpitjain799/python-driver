36a6b434c874738426dbe743dd180e83428f0d5b
weideng1 <weideng1@users.noreply.github.com>
2023-04-21 08:21:37 -0600

    go back to yesterday's version 9e18c5aa38330f57f34a27afe44af7dc55f66014

--- tests/integration/cqlengine/conftest.py
+++ tests/integration/cqlengine/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from tests.integration.cqlengine import setup_package, teardown_package
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def setup_and_teardown_packages():
     setup_package()
     yield
