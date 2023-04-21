8c60322ff421169ec8dafe7498e1561e388f2448
weideng1 <weideng1@users.noreply.github.com>
2023-04-21 00:21:11 -0600

    switch back to session scope for pytest fixture

--- tests/integration/cloud/conftest.py
+++ tests/integration/cloud/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from tests.integration.cloud import setup_package, teardown_package
 
-@pytest.fixture(scope='module', autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def setup_and_teardown_packages():
     setup_package()
     yield
